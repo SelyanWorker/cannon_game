d832dda981fc593e7d10ae98bb582c657ef0228d
Selyan <selyanworker@gmail.com>
2021-08-26 23:13:35 +0500

    Fixed problem with alpha (enabled GL_BLEND)

--- platforms/OpenGL/OGLRenderer.cpp
+++ platforms/OpenGL/OGLRenderer.cpp
@@ -10,6 +10,8 @@ namespace selyan
     {
         glClearColor(0.8f, 0.8f, 0.8f, 1.f);
         glEnable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     }
 
     void clear() { glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); }
